1160dcc78dd1a5b6b97d96ad81ad556949f99e9e
KarolWes <karol.wesolowski01@gmail.com>
2023-04-17 16:03:22 +0200

    init and UI

---
+++ app/src/main/java/com/example/bluetoothgame/ui/home/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.example.bluetoothgame.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.bluetoothgame.databinding.FragmentCurrentBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentCurrentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentCurrentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textTitle
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
